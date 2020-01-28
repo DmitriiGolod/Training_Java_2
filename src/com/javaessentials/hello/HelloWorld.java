c15056210e4ac1b0d3dbae9973e5606231034c42
Dmitrii_Golod <dmitry.golod@gmail.com>
2020-01-28 19:10:34 +0300

    Added output of one more string in the main.

--- src/com/javaessentials/hello/HelloWorld.java
+++ src/com/javaessentials/hello/HelloWorld.java
@@ -3,5 +3,6 @@ package com.javaessentials.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("One more thing!");
     }
 }
