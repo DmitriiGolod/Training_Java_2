d7c6ca690ad74ee84bd4e358553c57db3ee461f0
Dmitrii_Golod <dmitry.golod@gmail.com>
2020-01-29 14:06:16 +0300

    Added getenv call.

--- src/com/javaessentials/hello/HelloWorld.java
+++ src/com/javaessentials/hello/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("One more thing!");
+        String sJavaPath=System.getenv("JAVA_HOME");
+        System.out.println(sJavaPath);
     }
 }
